965aaba10758535b9dffe3cd17bbe36260ada5c4
sumitjangir179 <sumitjangir179@gmail.com>
2025-07-30 16:18:43 +0530

    feat: custom error response

---
+++ src/utils/ApiError.js
@@ -0,0 +1,17 @@
+class ApiError extends Error {
+    constructor(statusCode, message = 'something went wrong', errors = [], stack = '') {
+        super(message)
+        this.statusCode = statusCode
+        this.message = message
+        this.error = errors
+        this.data = null
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export default ApiError
