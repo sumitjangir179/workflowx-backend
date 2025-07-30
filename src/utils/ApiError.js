68ff2781ca27d7c9f6e93f265e5cbf3f2148d82d
sumitjangir179 <sumitjangir179@gmail.com>
2025-07-30 16:41:39 +0530

    refact: improve custom error class

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -2,7 +2,6 @@ class ApiError extends Error {
     constructor(statusCode, message = 'something went wrong', errors = [], stack = '') {
         super(message)
         this.statusCode = statusCode
-        this.message = message
         this.error = errors
         this.data = null
 
