fa56d8c4a23a20cd4f23c97de4fc48b5d91a2d80
unknown <sumitjangir179@gmail.com>
2025-09-05 15:46:46 +0530

    feat: project model

---
+++ src/models/project.model.js
@@ -0,0 +1,42 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../constants.js';
+import { use } from 'react';
+
+const ProjectSchema = sequelize.define(
+  'Project',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+    },
+  },
+
+  {
+    timestamps: true,
+  },
+);
+
+export default ProjectSchema;
