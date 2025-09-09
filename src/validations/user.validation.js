8c6d22c0bb93ee2d0ab220a81083e93e99416395
sumit jangir <sumitjangir179@gmail.com>
2025-09-09 21:47:47 +0530

    feat: add loginUserSchema and updateUserDetailsSchema

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -2,5 +2,17 @@ import z from 'zod';
 
 export const registerUserSchema = z.object({
   email: z.string().email('Invalid email address'),
-  password: z.string().min(6, 'Password must be at least 6 characters long').optional(),
+});
+
+export const loginUserSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters long'),
+});
+
+export const updateUserDetailsSchema = z.object({
+  name: z.string().min(3, 'Name must be at least 2 characters long').optional(),
+  password: z
+    .string()
+    .min(6, 'Password must be at least 6 characters long')
+    .optional(),
 });
