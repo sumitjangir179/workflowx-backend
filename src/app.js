56c22a1d56f927af7c652e826dbbcafb9c761a64
sumit jangir <sumitjangir179@gmail.com>
2025-09-29 09:49:01 +0530

    chore: add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json({limit: '16mb'}));
 app.use(cookieParser())
+app.use(cors({origin: process.env.CLIENT_ORIGIN, credentials: true}));
 
 
 // route declaration
